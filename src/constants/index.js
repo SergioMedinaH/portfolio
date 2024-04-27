9501d6e3e90ba6836042c125dbe9d51758ddf6d3
SERGIO MEDINA <sergiomher@gmail.com>
2024-04-27 17:00:47 +0200

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,348 @@
+import {
+    mobile,
+    backend,
+    creator,
+    web,
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    figma,
+    docker,
+    nextjs,
+    sanity,
+    stripe,
+    vercel,
+
+    meta,
+    ib,
+    upm,
+    unity,
+    starbucks,
+    tesla,
+    shopify,
+    carrent,
+    imagenportfolio,
+    jobit,
+    tripguide,
+    threejs,
+    logoport,
+    emptyprof,
+  } from "../assets";
+
+  
+  
+  export const navLinks = [
+    {
+      id: "about",
+      title: "Sobre mí",
+    },
+    {
+      id: "work",
+      title: "Experiencia",
+    },
+    {
+      id: "contact",
+      title: "Contacto",
+    },
+  ];
+  
+  const services = [
+    {
+      title: "Desarrollo de Aplicaciones Web",
+      icon: web,
+    },
+    {
+      title: "Desarrollo de Aplicaciones Móviles",
+      icon: mobile,
+    },
+    {
+      title: "Diseño Web desde Cero",
+      icon: backend,
+    },
+    {
+      title: "Creación de Elementos Gráficos 2D y 3D",
+      icon: creator,
+    },
+  ];
+  
+  const technologies = [
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    {
+      name: "TypeScript",
+      icon: typescript,
+    },
+    {
+      name: "React JS",
+      icon: reactjs,
+    },
+    {
+      name: "Next JS",
+      icon: nextjs,
+    },
+    {
+      name: "Tailwind CSS",
+      icon: tailwind,
+    },
+    {
+      name: "Node JS",
+      icon: nodejs,
+    },
+    {
+      name: "Sanity",
+      icon: sanity,
+    },
+    {
+      name: "Three JS",
+      icon: threejs,
+    },
+    {
+      name: "git",
+      icon: git,
+    },
+    {
+      name: "figma",
+      icon: figma,
+    },
+    {
+      name: "Stripe",
+      icon: stripe,
+    },
+  ];
+  
+  const experiences = [
+    {
+      title: "Desarrollador de Videojuegos con Unity",
+      company_name: "SMH Studios",
+      icon: unity,
+      iconBg: "#E6DEDD",
+      date: "Jul 2017 - Ene 2021",
+      points: [
+        "Primer contacto con la programación y el desarrollo de software utilizando C# y Unity.",
+        "He aprendido a resolver problemas y a 'buscarse la vida con internet', aprendiendo a ser autodidacta, permitiéndome resolver cualquier problema",
+        "He aprendido sobre diseño gráfico y 3D, utilizando Blender, GIMP, Inkscape, ...",
+        "Participación en 'Game Jams' (Concurso de desarrollar un videojuego en un corto periodo de tiempo), quedando segundo en la 'Redefine 2021 Game Jam'.",
+      ],
+    },
+    {
+      title: "Estudiante IB",
+      company_name: "Bachillerato Internacional",
+      icon: ib,
+      iconBg: "#E6DEDD",
+      date: "Sep 2021 - May 2023",
+      points: [
+        "Lorem ipsum dolor sit amet consectetur, adipiscing elit montes nulla feugiat, primis natoque non habitasse.",
+        "Lorem ipsum dolor sit amet consectetur, adipiscing elit est rutrum nisi lobortis, urna purus tristique curabitur.",
+        "Lorem ipsum dolor sit amet consectetur adipiscing, elit eleifend nam parturient per.",
+        "Lorem ipsum dolor sit amet consectetur adipiscing elit, aptent cum quam nec fermentum vitae.",
+      ],
+    },
+    {
+      title: "Aprendiz Autodidacta de Desarrollo Web y Móvil",
+      company_name: "Autodidacta",
+      icon: logoport,
+      iconBg: "#383E56",
+      date: "Feb 2020 - Presente",
+      points: [
+        "De forma autodidacta, he aprendido a crear aplicaciones web utilizando React.js, Tailwind.css, Next.js, Sanity.io, ..., pudiendo transformar cualquier diseño a una página web funcional y estética.",
+        "He aprendido a desarrollar aplicaciones móviles utilizando React Native, pudiendo crear aplicaciones nativas para Android y iOS obteniendo un gran rendimiento junto a un diseño moderno.",
+        "Utilizando las últimas tecnologías como Tailwind.css, Sanity.io, ..., permite crear diseños totalmente personalizados, poder editar la información de la aplicación web muy facilmente a través de una interfaz gráfica, permitiendo realizar cambios a tiempo real.",
+        "He aprendido a diseñar elementos graficos 2D (tanto vectorial como de imagen de píxeles) a través del uso de GIMP, Inkscape, Figma, Canva, Midjourney, como 3D utilizando Blender, pudiendo tanto modelar como renderizar objetos 3D."
+      ],
+    },
+    {
+      title: "Grado de Ciencia de Datos e Inteligencia Artificial",
+      company_name: "Universidad Politécnica de Madrid",
+      icon: upm,
+      iconBg: "#383E56",
+      date: "Sep 2023 - Presente",
+      points: [
+        "Lorem ipsum dolor sit amet consectetur, adipiscing elit montes nulla feugiat, primis natoque non habitasse.",
+        "Lorem ipsum dolor sit amet consectetur, adipiscing elit est rutrum nisi lobortis, urna purus tristique curabitur.",
+        "Lorem ipsum dolor sit amet consectetur adipiscing, elit eleifend nam parturient per.",
+        "Lorem ipsum dolor sit amet consectetur adipiscing elit, aptent cum quam nec fermentum vitae.",
+      ],
+    },
+  ];
+  
+  const testimonials = [
+    {
+      testimonial:
+        "Lorem ipsum dolor sit amet consectetur adipiscing elit curae tortor, mi est nisl sociosqu scelerisque duis etiam.",
+      name: "Elon Musk",
+      designation: "CEO",
+      company: "SpaceX",
+      image: emptyprof,
+    },
+    {
+      testimonial:
+        "Lorem ipsum dolor sit amet consectetur adipiscing, elit hac ut dictum mauris vehicula tincidunt, congue at sem ultrices dis.",
+      name: "Sudar Pichai",
+      designation: "CEO",
+      company: "Google",
+      image: emptyprof,
+    },
+    {
+      testimonial:
+        "Lorem ipsum dolor sit amet consectetur, adipiscing elit est rutrum nisi lobortis, urna purus tristique curabitur.",
+      name: "Andrew Bosworth",
+      designation: "CTO",
+      company: "Meta",
+      image: emptyprof,
+    },
+  ];
+  
+  const projects = [
+    
+    {
+      name: "Notion App Clone SaaS",
+      description:
+        "💵 Monthly Subscription System with Stripe \n ⏱️ Real-time Cursors and Collaboration \n 🔐 Custom Authentication with email 2FA \n 📝 Create, Update, Share and Delete Workspaces, Folders & Files \n 🌙 Light-Dark Mode \n 📱 Responsive design",
+      tags: [
+        {
+          name: "next.js",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "supabase",
+          color: "green-text-gradient",
+        },
+        {
+          name: "stripe",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: jobit,
+      source_code_link: "https://github.com/SergioMedinaH/notion-clone",
+      website_link: "https://notion-clone-production-778a.up.railway.app/"
+    },
+    {
+      name: "Events Platform",
+      description:
+        "🔐 Authentification with Clerk \n 💸 Checkout & Payments with Stripe \n 🔎 Search & Filter \n  🎫 Create, Read, Update & Delete Events \n 📊 Event Orders Management System",
+      tags: [
+        {
+          name: "next.js",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "mongodb",
+          color: "green-text-gradient",
+        },
+        {
+          name: "stripe",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: jobit,
+      source_code_link: "https://github.com/SergioMedinaH/event_platform",
+      website_link: "https://event-platform-one-sigma.vercel.app/"
+    },
+    {
+      name: "AI Image Manipulator SaaS Platform",
+      description:
+        "🔐 Authentification with Clerk \n 💸 Credits System & Purchase with Stripe \n ✨ Image Restoration, Recoloring, Generative Fill, Object Removal & Background Removal with AI \n 🔎 Advance AI Image Search & Filter \n ⬇️ Download and Save Transformed Images",
+      tags: [
+        {
+          name: "next.js",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "restapi",
+          color: "green-text-gradient",
+        },
+        {
+          name: "scss",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: jobit,
+      source_code_link: "https://github.com/SergioMedinaH/imaginify",
+      website_link: "https://imaginify-six-ebon.vercel.app/"
+    },
+    {
+      name: "IPhone Website",
+      description:
+      "🎥 3D Model Rendering with Different Colors and Sizes \n  ✨ Smooth Animations with GSAP \n 📱 Completely Responsive \n 🎠 Custom Video Carousel",
+      tags: [
+        {
+          name: "nextjs",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "firebase",
+          color: "green-text-gradient",
+        },
+        {
+          name: "css",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: tripguide,
+      source_code_link: "https://github.com/SergioMedinaH/iphone",
+      website_link: "https://iphone-tan.vercel.app/"
+    },
+    {
+      name: "Brainwave",
+      description:
+        "💡 Latest UI Trends \n 🎥 Parallax Animations \n 👀 Cool Gradients \n 📱 Completely Responsive ",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "vite",
+          color: "green-text-gradient",
+        },
+        {
+          name: "tailwind",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: imagenportfolio,
+      source_code_link: "https://github.com/SergioMedinaH/brainwave",
+      website_link: "https://brainwave-nine-kappa.vercel.app/"
+    },
+    {
+      name: "Portfolio Personal",
+      description:
+        "📦 3D Models & Designs with Three.js \n ✨ Interactive Experience with Smooth Animations \n 📱 Completely Responsive \n 🎨 Custom Design",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "three",
+          color: "green-text-gradient",
+        },
+        {
+          name: "tailwind",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: imagenportfolio,
+      source_code_link: "https://github.com/",
+      website_link: "https://notion-clone-production-778a.up.railway.app/"
+    },
+  ];
+  
+  export { services, technologies, experiences, testimonials, projects };
